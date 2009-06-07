e205a50d2d7372d98fd91c3f954c08569ecd8fba
Sandro Turriate <sandro.turriate@gmail.com>
2009-06-07 03:18:15 -0400

    add fixtures for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,16 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'tagooru'
 
+def fixture(filename)
+  filename = File.basename(filename)
+  file = File.join(File.dirname(__FILE__), "fixtures", filename)
+  if File.exist?(file)
+    fixture_store[filename] || fixture_store[filename] = File.read(file)
+  end
+end
+
+private
+
+def fixture_store
+  @fixture_store ||= {}
+end
