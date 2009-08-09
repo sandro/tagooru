0099781b7ce28fba2c559069c3cf5291f1f032a4
Sandro Turriate <sandro.turriate@gmail.com>
2009-08-09 02:49:53 -0400

    Replace playlist search with api search
    Playlist search now requires authentication, so I'm replacing it with
    the ajax api search. Currently I'm sending in the api key used in their
    example on the documentation page, we'll see how long that lasts.

--- lib/tagooru.rb
+++ lib/tagooru.rb
@@ -10,11 +10,28 @@ class Tagooru
 
   include HTTParty
   base_uri 'http://tagoo.ru'
-  default_params :for => :audio
+  default_params :for => :audio, :key => '74d8940f'
+  headers 'Referer' => 'http://tagoo.ru/en/webmaster.php?mode=json_api'
+  format :plain
+
+  # override method from HTTParty to prevent cookies from overriding other
+  # header data (httparty 0.4.4)
+  def self.perform_request(http_method, path, options) #:nodoc:
+    Request.new(http_method, path, default_options.dup.merge(options)).perform
+  end
 
   def self.search(query, page = 1)
-    playlist = get '/en/search_playlist.php', :query => {:search => query, :page => page}
-    Playlist.new(playlist).parse
+    response = get '/api_search.php', :query => {:search => query, :page => page, :seed => generate_seed}
+    data = Crack::JSON.parse(response.sub(/^.+= /, ''))
+    data['data'].map do |d|
+      Track.new d['title'], d['file_url']
+    end
+  end
+
+  # seed argument taken from search api
+  # http://tagoo.ru/script/api/search.js
+  def self.generate_seed
+    (rand * 100_000).to_i
   end
 
   def self.first(query, page = 1)
